d9d04af09b308194f27fa7e304e29e1969b3ab77
Lucas Greenwell <lucasgreenwell15@gmail.com>
2020-05-07 09:17:41 -0600

    took a test

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,54 @@
 // write your custom hook here to control your checkout form
+import { useLocalStorage } from "./useLocalStorage";
+
+   const useForm = (key, initialValue) => {
+  // email value ✅
+  // handlechanges should handle all inputs
+  const [values, setValues] = useLocalStorage(key, initialValue);
+
+  const handleChanges = e => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  // const handleBlur = e => {
+
+  // }
+
+  // const handleValidation
+
+  const clearForm = e => {
+    if (e) e.preventDefault();
+    setValues(initialValue);
+  };
+
+  return [values, handleChanges, clearForm];
+};
+export default useForm
+// JS destructuring -
+
+// destructure objects
+//   name matters
+//   order does not matter
+
+// const myObj = {
+//   a: "1",
+//   b: "2",
+//   c: "3"
+// };
+
+// const { b, c, a } = myObj;
+
+// console.log(b, c, a);
+
+// // destrcutring arrays
+// //   names don't matter
+// //   order matters
+
+// const myArr = ["1", "2", "3"];
+
+// const [banana, two, apple] = myArr;
+
+// console.log(banana, two, apple);
